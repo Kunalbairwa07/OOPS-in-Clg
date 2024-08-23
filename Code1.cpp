97501d67ab121bb64d7a38d9d2c08dd6d7244350
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2024-08-23 22:41:13 +0530

    oops Initial

---
+++ Code1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    double num1, num2;
+    double sum, difference, product, quotient;
+
+    cout << "enter the first no. =";
+    cin >> num1;
+
+    cout << "enter the second no. =";
+    cin >> num2;
+
+    sum = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+
+    cout << "\n result="<< sum << "\n difference=" << difference << "\n product=" << product << "\n" ; 
+
+
+    return 0;
+
+}
