77afe8b74bd6d80305eaf8fa58fafce9ad552682
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2024-08-24 00:52:36 +0530

    Relational operator.

---
+++ Code3.cpp
@@ -0,0 +1,18 @@
+/*
+Author:- Kunal Bairwa.
+Topic:- Relational operator.
+Created on :- 23/08/24.
+*/
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int a = 10;
+    int b = 6;
+    cout << "a>b:-" << (a>b)<<endl;
+    cout << "a<b:-" << (a<b) << endl;
+    cout << "a==b:-" << (a==b) << endl;
+    cout << "a!=b:-" << (a!=b) << endl;
+    return 0;
+}
