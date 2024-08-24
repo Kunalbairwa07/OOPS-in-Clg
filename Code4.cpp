82cecbec0ceb5e8a621d4671b048f89dffe89110
Kunalbairwa07 <kunalbairwa2580@gmail.com>
2024-08-24 20:56:50 +0530

    Bitwise operator..

--- Code4.cpp
+++ Code4.cpp
@@ -15,14 +15,14 @@ int main ()
     int bitwise_or = a/b;
     int bitwise_xor = a^b;
     int bitwise_not = ~a;
-    //int bitwise_shift = a<<2;
-    //int bitwise_shift = a>>1;
+    int bitwise_lshift = a<<2;
+    int bitwise_rshift = a>>1;
 
-    cout << "AND" << bitwise_and << endl;
-    cout << "OR" << bitwise_or << endl;
-    cout << "XOR" << bitwise_xor << endl;
-    cout << "NOTa" << bitwise_not << endl;
-    //cout << "Left shift" << bitwise_shift << endl;
-    //cout << "Right shift" << bitwise_shift << endl;
+    cout << "AND=" << bitwise_and << endl;
+    cout << "OR=" << bitwise_or << endl;
+    cout << "XOR=" << bitwise_xor << endl;
+    cout << "NOTa=" << bitwise_not << endl;
+    cout << "Left shift=" << bitwise_lshift << endl;
+    cout << "Right shift=" << bitwise_rshift << endl;
     return 0;
 }
